979b3feda1a3c9ad21f33b7210d3be77f46bd0bd
insd47 <me@insd.dev>
2023-10-03 23:18:09 +0900

    refactor: modulize main card

---
+++ src/components/pages/main/card/index.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+import { CommentMainCard, HeartMainCard } from "@/components/svg";
+import styles from "./styles.module.scss";
+
+export default function Card({ title, id, author, comments, hearts, thumbnail, iconColor, className, ...props }) {
+  return (
+    <Link href="/any" className={[styles.card, className].join(" ")} {...props}>
+      <div className={styles.image}>
+        <img alt="thumbnail" src={thumbnail ?? process.env.FALLBACK_THUMBNAIL_URL} />
+      </div>
+      <div className={styles.info}>
+        <p className={styles.title}>{title}</p>
+        <div>
+          <p>{author}</p>
+          <div className={styles.reactions}>
+            <div>
+              <CommentMainCard color={iconColor ?? "white"} />
+              <span>{comments}</span>
+            </div>
+            <div>
+              <HeartMainCard color={iconColor ?? "white"} />
+              <span>{hearts}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
