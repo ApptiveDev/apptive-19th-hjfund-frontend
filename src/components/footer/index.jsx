212a3ee01b82dab9f4702c6abaeefc0393876670
insd47 <me@insd.dev>
2024-01-01 10:31:36 +0900

    feat: footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,50 +1,17 @@
 import Link from "next/link";
 import styles from "./styles.module.scss";
-import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.grid}>
-        <div>
-          <Link href="/any" className={styles["link-big"]}>
-            커뮤니티
-          </Link>
-          <Link href="/any">종목</Link>
-          <Link href="/any">투자노트</Link>
-        </div>
-        <div>
-          <Link href="/any" className={styles["link-big"]}>
-            리포트 포스팅
-          </Link>
-          <Link href="/any">인베스팅</Link>
-          <Link href="/any">재무지식</Link>
-        </div>
-        <div>
-          <Link href="/any" className={styles["link-big"]}>
-            스크랩
-          </Link>
-          <Link href="/any">커뮤니티 스크랩</Link>
-          <Link href="/any">리포트 스크랩</Link>
-        </div>
-        <div>
-          <Link href="/any" className={styles["link-big"]}>
-            마이페이지
-          </Link>
-          <Link href="/any">프로필 수정</Link>
-          <Link href="/any">회원 탈퇴</Link>
-          <Link href="/any">이용약관 </Link>
-        </div>
-      </div>
-      <div className={styles.logo}>
-        <p>{process.env.WEBSITE_NAME}</p>
-        <img
-          alt="logo"
-          src="/images/logo/stocktree.png"
-          srcSet="/images/logo/stocktree@2x.png 2x, /images/logo/stocktree@3x.png 3x"
-          width={213}
-          height={48}
-        />
+      <div className={styles.container}>
+        <h2>StockTree 2023</h2>
+        <ul>
+          <li><Link href="/terms">이용약관</Link></li>
+          <li><Link href="/privacy">개인정보 처리방침</Link></li>
+          <li><Link href="/licenses">오픈소스 라이선스</Link></li>
+        </ul>
+        <p>문의: {process.env.NEXT_PUBLIC_CONTACT_EMAIL_ADDRESS}</p>
       </div>
     </footer>
   );
