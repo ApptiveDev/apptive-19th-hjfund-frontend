1147afa3e14db73b2e835018832eac2b638b2138
Jungyoon Moon <102630375+nnoonjy@users.noreply.github.com>
2024-01-23 21:28:00 +0900

    Nnoonjy/#28/종목요청 페이지 리워크 (#45)

--- src/app/(main)/(children)/request/page.js
+++ src/app/(main)/(children)/request/page.js
@@ -3,6 +3,9 @@ import styles from "./styles.module.scss";
 // import { searchIcon } from "../../../../public/images/icon/search.png";
 import { Search } from "@/components/header/components";
 import Icon from "@/components/icon";
+// import { searchIcon } from "../../../../public/images/icon/search.png";
+import { Search } from "@/components/header/components";
+import Icon from "@/components/icon";
 
 export default function Request() {
   return (
@@ -15,13 +18,13 @@ export default function Request() {
           <span>➌ 요청 완료</span>
         </div>
         <div className={styles.requestboxbody}>
-          <span className={styles.textbox}>
+          <p className={styles.textbox}>
             <span className={styles.boxtitle}>종목 선택</span>
             <span className={styles.boxsubtitle}>
               요청하려는 종목을 입력하세요.
             </span>
-          </span>
-          <span className={styles.searchbox}>
+          </p>
+          <p className={styles.searchbox}>
             <input
               className={styles.searchbox_input}
               type="text"
@@ -33,12 +36,10 @@ export default function Request() {
               iconType="magnifying-glass"
               // onClick={() => console.log("search")}
             />
-          </span>
+          </p>
         </div>
         <div className={styles.requestboxfooter}>
-          <Link href="/">
-            <button className={styles.tomainbutton}>메인 화면으로</button>
-          </Link>
+          <button className={styles.tomainbutton}>메인 화면으로</button>
           <Link href="/request/2">
             <button className={styles.nextbutton}>다음</button>
           </Link>
