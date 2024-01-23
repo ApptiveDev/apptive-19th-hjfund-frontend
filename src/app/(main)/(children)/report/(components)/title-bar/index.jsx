bcdf43432452e9ed15f34dbcd6bc200e3dea2355
Jungyoon Moon <nnoonjy@gmail.com>
2024-01-24 08:22:59 +0900

    fix : 오류수정 및 검색창 구현

--- src/app/(main)/(children)/report/(components)/title-bar/index.jsx
+++ src/app/(main)/(children)/report/(components)/title-bar/index.jsx
@@ -1,33 +1,35 @@
 import { classes } from "@/tools/classes";
-import { conditionalClass } from "@/tools/classes";
 import styles from "./styles.module.scss";
 import { useUserAgent } from "@/tools/user-agent";
-import React, { useState } from "react";
+import React from "react";
+import Icon from "@/components/icon";
 
 const Title = () => {
-  const [selected, setSelected] = useState("newest");
+  // const [selected, setSelected] = useState("newest");
 
-  const handleCategoryClick = (value) => {
-    setSelected(value);
-  };
+  // const handleCategoryClick = (value) => {
+  //   setSelected(value);
+  // };
 
   return (
     <div className={classes(styles.title)}>
       <h1>모든 리포트</h1>
       <span
-        onClick={() => handleCategoryClick("newest")}
+        // onClick={() => handleCategoryClick("newest")}
         className={[
           styles.category,
-          selected === "newsest" ? styles.selected : "",
+          // selected === "newsest" ?
+          styles.selected,
+          // : "",
         ].join(" ")}
       >
         최신순
       </span>
       <span
-        onClick={() => handleCategoryClick("hotest")}
+        // onClick={() => handleCategoryClick("hotest")}
         className={[
           styles.category,
-          selected === "hotest" ? styles.selected : "",
+          // selected === "hotest" ? styles.selected : "",
         ].join(" ")}
       >
         인기순
@@ -39,7 +41,21 @@ const Title = () => {
 const Search = () => {
   const { isMobile } = useUserAgent();
 
-  return <div className={classes(styles.search)}>searchBar</div>;
+  return (
+    <span className={styles.searchbox}>
+      <input
+        className={styles.searchbox_input}
+        type="text"
+        placeholder="종목 코드 검색"
+      />
+      <Icon
+        className={styles["button-invert-icon"]}
+        button
+        iconType="magnifying-glass"
+        // onClick={() => console.log("search")}
+      />
+    </span>
+  );
 };
 
 const TitleBar = () => {
